f1aebaad6d4b0350c879df6a2cee30ed0d2476f6
prasad-poul <143923694+prasad-poul@users.noreply.github.com>
2023-09-03 19:07:03 +0530

    hr

---
+++ src/Career.jsx
@@ -0,0 +1,241 @@
+import React from "react";
+import './Career.css';
+import Logo from './Abhyaz-logo.jpg'
+import Jim from './j-img.jpg';
+import { ImFacebook, ImInstagram, ImPinterest, ImTwitter } from 'react-icons/im';
+
+const Career = () => {
+    return(
+        <>
+            <div className="head">
+                <div className="logo">
+                    <img src={Logo} alt=""  />
+                </div>
+            </div>
+            <div className="join-c">
+                <div className="join">
+                    JOIN WITH US
+                </div>
+                <div className="j-img">
+                    <img src={Jim  } alt=""  />
+                </div>
+            </div>
+            <div className="opening">
+                <div className="o-t">
+                    Current Opportunities
+                </div>
+                <div className="cards">
+                    <div className="card-title">
+                        <h2>React.js Developer</h2>
+                    </div>
+                    <div className="o-details">
+                        <div className="id">
+                            <p>Job ID: <strong>JRE57888</strong></p>
+                        </div>
+                        <div className="o-feild">
+                            <p>Feild: <strong>Information Technology</strong></p>
+                        </div>
+                        <div className="o-dates">
+                            <p>Posted Date: <strong>07/07/2023</strong></p>
+                        </div>
+                    </div>
+                    <div className="o-jd">
+                        <div className="jd-title">
+                            <h4>
+                                JOB DISCRIPTION
+                            </h4>
+                        </div>
+                        <div className="jd-det">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vel possimus asperiores aspernatur aperiam quia soluta tempora praesentium mollitia inventore?</p>
+                        </div>
+                    </div>
+                    <div className="a-p">
+                        <button>
+                            Apply Now!
+                        </button>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <div className="card-title">
+                        <h2>Full-Stack Developer</h2>
+                    </div>
+                    <div className="o-details">
+                        <div className="id">
+                            <p>Job ID: <strong>JRE57888</strong></p>
+                        </div>
+                        <div className="o-feild">
+                            <p>Feild: <strong>Information Technology</strong></p>
+                        </div>
+                        <div className="o-dates">
+                            <p>Posted Date: <strong>07/07/2023</strong></p>
+                        </div>
+                    </div>
+                    <div className="o-jd">
+                        <div className="jd-title">
+                            <h4>
+                                JOB DISCRIPTION
+                            </h4>
+                        </div>
+                        <div className="jd-det">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vel possimus asperiores aspernatur aperiam quia soluta tempora praesentium mollitia inventore?</p>
+                        </div>
+                    </div>
+                    <div className="a-p">
+                        <button>
+                            Apply Now!
+                        </button>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <div className="card-title">
+                        <h2>Marketing Excutive</h2>
+                    </div>
+                    <div className="o-details">
+                        <div className="id">
+                            <p>Job ID: <strong>JRE57888</strong></p>
+                        </div>
+                        <div className="o-feild">
+                            <p>Feild: <strong>Sales</strong></p>
+                        </div>
+                        <div className="o-dates">
+                            <p>Posted Date: <strong>07/07/2023</strong></p>
+                        </div>
+                    </div>
+                    <div className="o-jd">
+                        <div className="jd-title">
+                            <h4>
+                                JOB DISCRIPTION
+                            </h4>
+                        </div>
+                        <div className="jd-det">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vel possimus asperiores aspernatur aperiam quia soluta tempora praesentium mollitia inventore?</p>
+                        </div>
+                    </div>
+                    <div className="a-p">
+                        <button>
+                            Apply Now!
+                        </button>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <div className="card-title">
+                        <h2>Software engineer</h2>
+                    </div>
+                    <div className="o-details">
+                        <div className="id">
+                            <p>Job ID: <strong>JRE57888</strong></p>
+                        </div>
+                        <div className="o-feild">
+                            <p>Feild: <strong>Information Technology</strong></p>
+                        </div>
+                        <div className="o-dates">
+                            <p>Posted Date: <strong>07/07/2023</strong></p>
+                        </div>
+                    </div>
+                    <div className="o-jd">
+                        <div className="jd-title">
+                            <h4>
+                                JOB DISCRIPTION
+                            </h4>
+                        </div>
+                        <div className="jd-det">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vel possimus asperiores aspernatur aperiam quia soluta tempora praesentium mollitia inventore?</p>
+                        </div>
+                    </div>
+                    <div className="a-p">
+                        <button>
+                            Apply Now!
+                        </button>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <div className="card-title">
+                        <h2>Digital Media Intern</h2>
+                    </div>
+                    <div className="o-details">
+                        <div className="id">
+                            <p>Job ID: <strong>JRE57888</strong></p>
+                        </div>
+                        <div className="o-feild">
+                            <p>Feild: <strong>Information Technology</strong></p>
+                        </div>
+                        <div className="o-dates">
+                            <p>Posted Date: <strong>07/07/2023</strong></p>
+                        </div>
+                    </div>
+                    <div className="o-jd">
+                        <div className="jd-title">
+                            <h4>
+                                JOB DISCRIPTION
+                            </h4>
+                        </div>
+                        <div className="jd-det">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vel possimus asperiores aspernatur aperiam quia soluta tempora praesentium mollitia inventore?</p>
+                        </div>
+                    </div>
+                    <div className="a-p">
+                        <button>
+                            Apply Now!
+                        </button>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <div className="card-title">
+                        <h2>PHP Developer</h2>
+                    </div>
+                    <div className="o-details">
+                        <div className="id">
+                            <p>Job ID: <strong>JRE57888</strong></p>
+                        </div>
+                        <div className="o-feild">
+                            <p>Feild: <strong>Information Technology</strong></p>
+                        </div>
+                        <div className="o-dates">
+                            <p>Posted Date: <strong>07/07/2023</strong></p>
+                        </div>
+                    </div>
+                    <div className="o-jd">
+                        <div className="jd-title">
+                            <h4>
+                                JOB DISCRIPTION
+                            </h4>
+                        </div>
+                        <div className="jd-det">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi vel possimus asperiores aspernatur aperiam quia soluta tempora praesentium mollitia inventore?</p>
+                        </div>
+                    </div>
+                    <div className="a-p">
+                        <button>
+                            Apply Now!
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+            <footer className="bottom">
+                    <div className="cop">
+                        <p className="copy">Copyright &copy; 2023 Abyaz.</p>
+                    </div>
+                    <div className="social">
+                        <div className="soci">
+                            <div><ImTwitter /></div>
+                            <div><ImPinterest/></div>
+                            <div><ImFacebook /></div>
+                            <div><ImInstagram /></div>
+                        </div> 
+                    </div>
+                    <div className="design">
+                         Design & Developed by Abhyaz
+                    </div>
+            </footer>
+        </>
+    )
+};
+
+export default Career;
